c991aa381bba73758a51f3ab94fce17542fee6d7
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-03-12 16:46:09 +0100

    print name

--- modules/search.mjs
+++ modules/search.mjs
@@ -4,6 +4,8 @@ export function search(input,apiKey) {
 
     input.addEventListener('keypress', (event) => {
         if (event.key === 'Enter' && input.value!= '') {
+            let result = document.getElementById("foundProducts")
+            result.innerHTML = '';
             
             fetch("http://localhost:8080/api/v1/movie/search/"+ input.value, {
                 method: "GET",
@@ -13,12 +15,12 @@ export function search(input,apiKey) {
             .then(response => response.json())
             .then(data => {
                 console.log( data);
-/*                 let result = document.getElementById("foundProducts")
                 for (let i in data) {
                    let li = document.createElement("li");
-                   li.innerHTML = data[i].title;
+                   li.innerHTML = data[i].name;
+                   result.appendChild(li);
 
-                } */
+                }
             })
             input.value = '';
         }
