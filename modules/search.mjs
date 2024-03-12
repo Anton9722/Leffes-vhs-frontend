cc0c7b57e3610ec759df8a149f25585101eca06d
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-03-12 15:21:37 +0100

    get json

--- modules/search.mjs
+++ modules/search.mjs
@@ -1,26 +1,22 @@
-let searchValue = "";
+
 export function search(input,apiKey) {
     
 
     input.addEventListener('keypress', (event) => {
         if (event.key === 'Enter') {
-            searchValue = input.value;
             
+            fetch("http://localhost:8080/api/v1/movie/search/"+ input.value, {
+                method: "GET",
+                headers: apiKey,
+                "Content-Type": "application/json"
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log( data);
+            })
             input.value = '';
         }
     });
 
-    fetch("http://localhost:8080/api/v1/movie", {
-        method: "GET",
-        headers: apiKey,
-        "Content-Type": "application/json"
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log( data);
-    })
-    .catch(error => {
-        console.error("There was a problem with the fetch operation:", error);
-    });
 }
 
