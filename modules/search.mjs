1b2e5188c338ca50ef86861a2673fe303c6208a9
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-03-13 11:34:33 +0100

    felhantering

--- modules/search.mjs
+++ modules/search.mjs
@@ -4,27 +4,46 @@ export function search(input,apiKey) {
 
     input.addEventListener('keypress', (event) => {
         if (event.key === 'Enter' && input.value!= '') {
-            let result = document.getElementById("foundProducts")
-            result.innerHTML = '';
             
             fetch("http://localhost:8080/api/v1/movie/search/"+ input.value, {
                 method: "GET",
                 headers: apiKey,
                 "Content-Type": "application/json"
             })
-            .then(response => response.json())
-            .then(data => {
-                console.log( data);
-                for (let i in data) {
-                   let li = document.createElement("li");
-                   li.innerHTML = data[i].name;
-                   result.appendChild(li);
+            .then(response => response.json().catch(err =>{
+                let fp =document.getElementById("foundProducts")
+                fp.innerHTML = "";
+                let errMessage = document.createElement("li")
+                errMessage.innerText = "Inget resultat";
+                fp.appendChild(errMessage);
+    
 
+            }))
+            .then(data => {
+                if (!data.length == 0) {
+                    presentResult(data);
                 }
+                    
+
             })
             input.value = '';
         }
     });
+    
+/*     
+     */
+}
 
+function presentResult(produkts){
+    let result = document.getElementById("foundProducts")
+    result.innerHTML = '';
+    for (let i in produkts) {
+        console.log(produkts[i]);
+        let li = document.createElement("li");
+        li.innerHTML = produkts[i].name;
+        result.appendChild(li);
+        
+    }
+    
 }
 
