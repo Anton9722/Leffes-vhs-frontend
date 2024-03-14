0d23e18a85c5ab55a07819ec0a80de384de8b1d3
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-03-14 13:39:01 +0100

    WIP popular products

--- modules/index.mjs
+++ modules/index.mjs
@@ -1,21 +1,26 @@
 import { presentResult } from "./search.mjs";
 
-
-
 export function popularMovies(apiKey) {
-    fetch("http://localhost:8080/api/v1/movie", {
-        method: "GET",
-        headers: apiKey,
-        "Content-Type": "application/json"
-    })
-    .then(response => response.json().catch(err =>{
-        let fp =document.getElementById("foundProducts")
+  fetch("http://localhost:8080/api/v1/movie", {
+    method: "GET",
+    headers: apiKey,
+    "Content-Type": "application/json",
+  })
+    .then((response) =>
+      response.json().catch((err) => {
+        let fp = document.getElementById("foundProducts");
         fp.innerHTML = "";
-        let errMessage = document.createElement("li")
+        let errMessage = document.createElement("li");
         errMessage.innerText = "Inget resultat";
         fp.appendChild(errMessage);
-        
-        
-    }))
-    .then((data) => presentResult(data));
+      })
+    )
+    .then((data) => {
+      const firstFive = data.slice(0, 6);
+      console.log(firstFive);
+
+      if (window.location.href == "http://localhost:5500/index.html") {
+        presentResult(firstFive);
+      }
+    });
 }
