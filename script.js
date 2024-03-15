0147e0b46ab2bebfd87b16a61e8fb3474e2cfef5
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-03-15 10:16:08 +0100

    add to cart

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 import { search } from "./modules/search.mjs";
+import{addToCart} from "./modules/addToCart.mjs"
+
+let hs = document.getElementById("heroSection");
 
 const apiKey = new Headers();
 apiKey.append("api_key", "Leffes_api_nyckel");
 
 search(searchInput, apiKey);
+
+let bosse =document.createElement("button");
+bosse.innerText = "test knapp";
+bosse.addEventListener("click",()=>{
+    addToCart(3);
+})
+hs.appendChild(bosse);
+
 
 
