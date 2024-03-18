882351524afd7900b32b312c9f523c527cd8991b
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-03-18 11:26:32 +0100

    .

--- modules/index.mjs
+++ modules/index.mjs
@@ -17,7 +17,7 @@ export function popularMovies(apiKey) {
     )
     .then((data) => {
       let randomData = shuffleArray(data);
-      let firstFive = randomData.slice(0, 6);
+      let firstFive = randomData.slice(0, 7);
       console.log(firstFive);
 
       
